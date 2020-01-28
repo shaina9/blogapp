ee31e4995c81ed83936e9a42906adfd5637ff74c
Shaina <shainakhan763@gmail.com>
2020-01-28 14:04:53 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
